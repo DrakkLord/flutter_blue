3797569638c912e5e37b8f7094e5409058fdd86b
Németh Ádám <DrakkLord@users.noreply.github.com>
2019-05-28 11:46:36 +0200

    consolidate ios scan behaviour

--- ios/Classes/FlutterBluePlugin.m
+++ ios/Classes/FlutterBluePlugin.m
@@ -123,7 +123,9 @@ typedef NS_ENUM(NSUInteger, LogLevel) {
       uuids = [uuids arrayByAddingObject:[CBUUID UUIDWithString:u]];
     }
     // TODO: iOS Scan Options (#35)
-    [self->_centralManager scanForPeripheralsWithServices:uuids options:nil];
+    // NOTE(drakklord): this is here to ensure that the behaviour is the same as on android
+    NSDictionary *options = @{ CBCentralManagerScanOptionAllowDuplicatesKey : @(YES) };
+    [self->_centralManager scanForPeripheralsWithServices:uuids options: options];
     result(nil);
   } else if([@"stopScan" isEqualToString:call.method]) {
     [self->_centralManager stopScan];
